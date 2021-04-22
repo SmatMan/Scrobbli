37b6ad0f730bbdd238345788d73c3d877c47aed3
SmatMan <gamingajun10@icloud.com>
2021-04-22 17:11:02 -0400

    add a comment

--- src/config.example.js
+++ src/config.example.js
@@ -1,3 +1,7 @@
+// CHANGE THIS FILE'S NAME TO "config.js" without the quotes
+// Make sure to add your Last.FM API key from here: https://www.last.fm/api/account/create
+// You need to have some scrobbles on your Last.FM Account to use this
+
 const LastFMkey = "API KEY HERE";
 
 export { LastFMkey };
