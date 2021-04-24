b1af5b312eb2bbdbc95cd4875480b016fe820125
SmatMan <gamingajun10@icloud.com>
2021-04-23 23:09:20 -0400

    create about page

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <div className="about-group">
+        <h1 className="about-header">
+          scrobbli is a simplified Last.FM browser made for music-lovers by
+          music-lovers. that's it.
+        </h1>
+      </div>
+      <a className="about-github" href="https://github.com/SmatMan/scrobbli">
+        github.
+      </a>
+    </div>
+  );
+};
+
+export default About;
